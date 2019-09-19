d5b8491d14cdd7c6f79dcb35852c5b011f747ec6
vishang soni <vishangsoni@gmail.com>
2019-09-19 11:38:04 -0400

    cleaning up the create-react-app strucutre and adding components which draw a boxes on screen

---
+++ src/components/Game/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './style.css'
+
+function Game() {
+    return <div className="game">
+
+        <div className="row">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+        </div>
+
+        <div className="row">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+        </div>
+
+        <div className="row">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+        </div>
+        
+    </div>
+}
+
+export default Game
